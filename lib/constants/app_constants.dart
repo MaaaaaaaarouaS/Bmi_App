2cc849b5b84f7c9ce12c64f70d709cb5c8e97a54
SSSMMM-hub <maroi.sab17@gmail.com>
2021-04-26 22:31:19 +0000

    By Maroua

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#F0FFFF");
+final Color accentHexColor = HexColor("#008080");
